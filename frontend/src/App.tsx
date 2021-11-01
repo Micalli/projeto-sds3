3831a40052a9df6b180f80b99fc4d2d697995fea
[Bruno] <[bruno.micalli@uol.com.br]>
2021-11-01 13:24:30 -0300

    Charts estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,16 +8,13 @@ function App() {
   return (
     <>
       <NavBar />
-
       <div className="container">
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
         <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
             <BarChart />
           </div>
-
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Todas Vendas</h5>
             <DonutChart />
@@ -26,7 +23,6 @@ function App() {
         <div className="py-3">
           <h1 className="text-primary"></h1>
         </div>
-
         <DataTable />
       </div>
       <Footer />
