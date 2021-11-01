ad731c1a269b8076162dd71cd34149d8c3612288
[Bruno] <[bruno.micalli@uol.com.br]>
2021-11-01 12:31:35 -0300

    Componentes staticos feitos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
 
 function App() {
   return (
-    <div>
-     <h1 className="text-primary">dasdsa</h1>
-    </div>
+    <>
+      <NavBar />
+
+      <div className="container">
+        <h1 className="text-primary">dasdsa</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
