c847cc2c92946133933471c27a7ad68b1daa891c
[Bruno] <[bruno.micalli@uol.com.br]>
2021-11-05 18:53:36 -0300

    ENV var

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,73 +1,70 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Chart from 'react-apexcharts'
-import { SaleSuccess } from 'Types/Sale';
-import { round } from 'Utils/format';
-import { BASE_URL } from 'Utils/request';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import { SaleSuccess } from "Types/Sale";
+import { round } from "Utils/format";
+import { BASE_URL } from "Utils/request";
 
 type SeriesData = {
-    name:String;
-    data: number[];
-}
+  name: String;
+  data: number[];
+};
 
 type ChartData = {
-    labels:{
-        categories:string[];
-    }
-    series: SeriesData[]
-}
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
+};
 
 function BarChart() {
-
-    const [chartData, setChartData] = useState<ChartData>({ 
-        labels: {
-        categories: []
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: [],
     },
     series: [
-        {
-            name: "",
-            data: []                   
-        }
-    ]});
+      {
+        name: "",
+        data: [],
+      },
+    ],
+  });
 
-    useEffect(() => {
-        axios.get(`${BASE_URL}/sales/success-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSuccess[];
-            const myLabels = data.map((x) => x.sellerName);
-            const mySeries = data.map((x) => round((100.0 * x.deals) / x.visited, 1));
-            setChartData({ 
-                labels: {
-                categories: myLabels,
-            },
-            series: [
-                {
-                    name: "% Sucesso",
-                    data: mySeries,                  
-                }
-            ]});
-        })     
-    },[])
-
-    const options = {
-        plotOptions: {
-            bar: {
-                horizontal: true,
-            }
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
+      const data = response.data as SaleSuccess[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => round((100.0 * x.deals) / x.visited, 1));
+      setChartData({
+        labels: {
+          categories: myLabels,
         },
-    };
-    
-    return (
-        <Chart
-            options={{ ...options,xaxis:chartData.labels}}
-            series={chartData.series}
-            type="bar"
-            height="240"
-        />
-        
-    );
-  }
-  
-  export default BarChart;
-  
+        series: [
+          {
+            name: "% Sucesso",
+            data: mySeries,
+          },
+        ],
+      });
+    });
+  }, []);
+
+  const options = {
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+  };
+
+  return (
+    <Chart
+      options={{ ...options, xaxis: chartData.labels }}
+      series={chartData.series}
+      type="bar"
+      height="240"
+    />
+  );
+}
+
+export default BarChart;
