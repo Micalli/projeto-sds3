914b70746bbc3391654a08ecf6ef02d7fec5ade7
[Bruno] <[bruno.micalli@uol.com.br]>
2021-11-05 16:43:27 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,56 +1,53 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "Types/Sale";
+import { formatLocalDate } from "Utils/format";
+import { BASE_URl } from "Utils/request";
+
 function DataTable() {
-    return (
-        <div className="table-responsive">
-    <table className="table table-striped table-sm">
+  const [page, setPage] = useState<SalePage>({
+    first: true,
+    last: true,
+    number: 0,
+    totalElements: 0,
+    totalPages: 0,
+  });
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URl}/sales?page=0&size=20&sort=date,desc`)
+      .then((response) => {
+        setPage(response.data);
+      });
+  }, []);
+  return (
+    <div className="table-responsive">
+      <table className="table table-striped table-sm">
         <thead>
-            <tr>
-                <th>Data</th>
-                <th>Vendedor</th>
-                <th>Clientes visitados</th>
-                <th>Negócios fechados</th>
-                <th>Valor</th>
-            </tr>
+          <tr>
+            <th>Data</th>
+            <th>Vendedor</th>
+            <th>Clientes visitados</th>
+            <th>Negócios fechados</th>
+            <th>Valor</th>
+          </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
+          {page.content?.map(x =>(
+            <tr key ={x.id}>
+              <td>{formatLocalDate(x.date, "dd/MM/yyyy")}</td>
+              <td>{x.seller.name}</td>
+              <td>{x.visited}</td>
+              <td>{x.deals}</td>
+              <td>{x.amount.toFixed(2)}</td>
             </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
+          ))}
+            
+          
         </tbody>
-    </table>
-</div>
-    );
-  }
-  
-  export default DataTable;
+      </table>
+    </div>
+  );
+}
+
+export default DataTable;
