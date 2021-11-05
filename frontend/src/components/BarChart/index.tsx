8e15345897aa7b8c513ce9bee808b175f324072b
[Bruno] <[bruno.micalli@uol.com.br]>
2021-11-05 18:23:59 -0300

    ENV var

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -35,8 +35,8 @@ function BarChart() {
         axios.get(`${BASE_URL}/sales/success-by-seller`)
         .then(response => {
             const data = response.data as SaleSuccess[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => round(100.0 * x.deals / x.visited,1));
+            const myLabels = data.map((x) => x.sellerName);
+            const mySeries = data.map((x) => round((100.0 * x.deals) / x.visited, 1));
             setChartData({ 
                 labels: {
                 categories: myLabels,
